17ec418b8c90a8ab3d4e559b3510623ae4a81e1c
nierunjie <nierunjie@gmail.com>
2018-11-20 18:09:24 +0800

    update

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ logging.basicConfig(level=logging.ERROR,
                     format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 
 if __name__ == '__main__':
+    logging.info('admin bot started')
     token = sys.argv[1]
     bot = AdminBot(token)
     bot.run()
