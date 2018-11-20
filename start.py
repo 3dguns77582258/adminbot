ce385311a69541e7d1fffa7272c147c1fd1a6cea
nierunjie <nierunjie@gmail.com>
2018-11-20 19:48:12 +0800

    update

--- start.py
+++ start.py
@@ -1,7 +1,8 @@
 def start(bot, update):
     chat_id = update.message.chat_id
     text = '欢迎使用，玩的开心\n'
-    text += '回复想要被投票移除的成员，回复内容为 /kick \n'
+    text += '回复想要被投票移除的成员发送的消息，回复内容为 /kick \n'
+    text += '此机器人附带删除入群退群通知 ，不要忘记给权限哦\n'
     text += '源码：<a href="https://github.com/nierunjie/adminbot">Github</a>，疯狂暗示小星星\n'
     text += '作者：@Lanthora\n'
     bot.send_message(chat_id, text,
